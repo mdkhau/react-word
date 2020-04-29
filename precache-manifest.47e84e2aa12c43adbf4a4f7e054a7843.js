ce88b36efe3aacbe71cdd17dc1dd45c2db0fa2e6
kshuvogit <git>
2020-04-30 00:05:26 +0300

    Updates

---
+++ precache-manifest.47e84e2aa12c43adbf4a4f7e054a7843.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31387137e4e2a4153d95f0d038727963",
+    "url": "/word-app/index.html"
+  },
+  {
+    "revision": "d62e6d3ef4840e0c71bc",
+    "url": "/word-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "95b17192381531a03daf",
+    "url": "/word-app/static/js/2.4f488296.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/word-app/static/js/2.4f488296.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d62e6d3ef4840e0c71bc",
+    "url": "/word-app/static/js/main.97c3a2fa.chunk.js"
+  },
+  {
+    "revision": "3fb71b8c1d3a4eb85128",
+    "url": "/word-app/static/js/runtime-main.b8654bac.js"
+  }
+]);
